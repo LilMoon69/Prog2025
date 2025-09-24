a6c9b588b2ee9d8f046d04adc378f8a443ba1af3
Dani <zgdani20@gmail.com>
2025-09-24 08:38:04 +0200

    kuki

--- elso.py
+++ elso.py
@@ -11,4 +11,27 @@ for i in range(100):
 print(szamok)
 jatek_szam = 0
 nem_talaltDB= 0
-kitalalndo_szam= random.randint(1,100)
+kitalalndo_szam=szamok[random.randint(0,len(szamok))]
+tipp=input("Kérek egy egész számot 1 és 100 között: ")
+
+while(not tipp.isdecimal()):
+    print("Egész számot adj meg!")
+    tipp=input("Kérek egy egész számot 1 és 100 között: ")
+tipp=int(tipp)
+if (tipp<kitalalndo_szam):
+    print("A tippelt szám kisebb, mint a kitalálandó szám.")
+elif (tipp<kitalalndo_szam):
+    print("A tippelt szám nagyobb, mint a kitalálandó szám.")
+while(tipp!=kitalalndo_szam):
+    tipp=input("Kérek egy egész számot 1 és 100 között: ")
+    while(not tipp.isdecimal()):
+        print("Egész számot adj meg!")
+        tipp=input("Kérek egy egész számot 1 és 100 között: ")
+    tipp=int(tipp)
+    if (tipp<kitalalndo_szam):
+        print("A tippelt szám kisebb, mint a kitalálandó szám.")
+    elif (tipp<kitalalndo_szam):
+        print("A tippelt szám nagyobb, mint a kitalálandó szám.")
+        
+print("Gratulálok, kitaláltad a számot!")
+    
